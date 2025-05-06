08fdb3b27a059a32b35ce52bb2841fdcc3a0b0f7
Danar09022006 <rohmanrozaqi8@gmail.com>
2025-05-06 12:32:13 +0700

    memasukkan input

--- singly.linkedlist.cpp
+++ singly.linkedlist.cpp
@@ -109,20 +109,20 @@ void traverse()
 int main()
 {
  
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
+    while(1)
+    {
+     cout << endl
+          << "Menu";
+     cout << endl
+          << "1. Menambah data kedalam list" << endl;
+     cout << "2. Menghapus data dari dalam list" << endl;
+     cout << "3. Menampilkan semua data didalam list" << endl;
+     cout << "4. Mencari data dalam list" << endl;
+     cout << "5. keluar" << endl;
+     cout << endl
+          << "Masukkan pilihan (1-5) : ";
+     cin >> ch;
+     switch (ch)
  
  
   {
@@ -138,12 +138,12 @@ int main()
         cout << endl << "list Kosong" << endl;
         break;
       }
-      cout << endl << "\nMasukkna no Mahasiswa yang akan dihapus : ";
-      cin >> nim;
-      if (mhs.delNode(nim) == false)
-        cout << endl << "Data tidak ditemukan" << endl;
-      else
-        cout << endl << "Data dengan nomor Mahasiswa" << nim << "berhasil dihapus " << endl;
+      
+      
+      
+      
+      
+      
     }
     break;
     case '3': 
