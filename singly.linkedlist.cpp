52a9224beb39b526a4c4d44073ae0c4805a9537b
Danar09022006 <rohmanrozaqi8@gmail.com>
2025-05-06 11:08:12 +0700

    multifire

--- singly.linkedlist.cpp
+++ singly.linkedlist.cpp
@@ -10,15 +10,14 @@ public:
 
 };
 
-class LinkedList
-{
-    Node *START;
 
-public:
-    LinkedList()
-    {
-        START = NULL;
-    }
+
+
+
+
+
+
+
 
     void addNode()
     {
@@ -39,5 +38,17 @@ public:
         }   nodeBaru->next = START;
             START = nodeBaru;
     }       return;
-    
+
+        Node *previous = START;
+        Node *current = START;
+        while ((current != NULL) && (nim >= current->noMhs))
+        {
+            if (nim == current->noMhs)
+            {
+                cout << "\nDuplikasi noMhs tidak diijinkan\n";
+                return;
+            }
+        }   previous = current;
+        current = current->next;
+
 }
