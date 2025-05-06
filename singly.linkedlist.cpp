f8dcd13d7ed045cd7daf438233636e9524abf09c
Danar09022006 <rohmanrozaqi8@gmail.com>
2025-05-06 12:13:42 +0700

    pussh

--- singly.linkedlist.cpp
+++ singly.linkedlist.cpp
@@ -10,14 +10,15 @@ public:
 
 };
 
+class LinkedList
+{
+    Node *START;
 
-
-
-
-
-
-
-
+public:
+    LinkedList()
+    {
+        START = NULL;
+    }
 
     void addNode()
     {
@@ -47,8 +48,164 @@ public:
             {
                 cout << "\nDuplikasi noMhs tidak diijinkan\n";
                 return;
+        
             }
-        }   previous = current;
-        current = current->next;
+            previous = current;
+            current = current->next;
+        
+        }
+        nodeBaru->next = current;
+        previous->next = nodeBaru;
+        {
+            bool search(int nim, Node **previous, Node **current)
+            {
+                *previous = START;
+                current = START;
 
+                while ((*current != NULL) && (nim != (*current)->noMhs))
+                {
+                    *previous = *current;
+                    *current = (*current)->next;
+                }
+                return (*current != NULL);
+            
+            
+            
+            }   
+            bool delNode(int nim)
+            {
+                Node *current, *previous;
+                if (!search(nim, &previous, &current))
+                return false;
+
+                if (current == START)
+                START = START->next;
+                else
+                    previous->next = current->next;
+
+                delete current;
+                return true;
+            
+            }   
+        }
+        void traverse()
+        {
+            if (listEmpty())
+            {
+                cout << "\nList kosong\n";
+            }
+            else
+            {
+                cout << "\nData dalam list adalah:\n";
+                Node *currentNode = START;
+                while (currentNode != NULL)
+                {
+                    cout << currentNode->noMhs << endl;
+                    currentNode = currentNode->next;
+
+                }
+                cout << endl;
+            }   
+        
+        
+     }   
+
+};
+
+int main()
+{
+    LinkedList mhs;
+    int nim;
+    char ch;
+    while (1)
+    {
+        cout << endl;
+        cout << "Menu";
+        cout << endl;
+             << "1. Menambah data kedalam list" << endl;
+        cout << "2. Menghapus data dari dalam list" << endl;
+        cout << "3. Menampilkan semua data didalam list" << endl;
+        cout << "4. Mencari data dalam list" << endl;
+        cout << "5. Keluar" << endl;
+        cout << endl;
+        cout << "Pilih menu: ";
+             << "Masukkan pilihan (1-5): ";
+        cin >> ch;
+        switch (ch)
+        {
+        case '1';
+        {
+            mhs.addNode();
+        }
+        break;
+             
+              case '2';
+            {
+                if (mhs.ListEmpty)()
+            {
+                cout << endl;
+                     << "List kosong " << endl;
+                break;
+
+            }
+            cout << endl;
+                 << "\nMasukkan no mahasiswa yang akan dihapus : ";
+            cin >> nim;
+            if (mhs.delNode(nim) == false)
+                cout << endl;
+                     << "Data tidak ditemukan" << endl;
+            else
+            cout << endl;
+                    << "Data dengan nomor mahasiswa " << nim << " berhasil dihapus " << endl;
+                       
+            
+            break;
+            case '3';
+            {
+                mhs.traverse();
+            }
+            break;
+            case '4';
+            {
+                if (mhs.listEmpaty() == true)
+                {
+                    cout << "\nList kosong\n";
+                    break;
+                }
+                Node *previous, current;
+                cout << endl;
+                     << "Masukkan no mahasiswa yang dicari : ";
+                cin >> nim;
+                if (mhs.search(nim, &previous, &current) == false)
+                    cout << endl;
+                         << "Data tiddak ditemukan" << endl;
+                else
+                {
+                    cout << endl;
+                        << "Data ditemukan" << endl;
+                    cout << "\nNo Mahasiswa: " << current->noMhs << endl;
+                    cout << "\n";
+                }
+
+           
+           
+           
+            }
+            break;
+            case '5';
+            {
+                exit(0);
+            }
+            break;
+            default:
+            {
+                cout << "Pilihan salah !." << endl;
+            }
+            break;
+
+            }
+
+ }  }  
+     
+    
 }
